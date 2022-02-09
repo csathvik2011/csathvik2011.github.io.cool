a1cd4dd8be23a6bf543d24d83a976e92e2bbc393
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-02-08 19:55:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var block = "document.getElementbyId"("block");
-var hole = "document.getElementbyId"("block");
+var hole = "document.getElementbyId"("hole");
+var character = "document.getElementbyId"("character")
+
 hole.addEvent('animationiteration', () => {
-var random = -((Math.random()*300)+150;
-hole.style.top = random + "px";
+var random = Math.random()*3;
+var top = (random*100)+150;
+hole.style.top = -(top)+"px";
 });
+setInterval(function)){
+var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  character.style.top = (charcterTop+3)+"px";
+},10);   
+
+function jump(){
+
+}
